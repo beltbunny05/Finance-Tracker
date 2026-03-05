1b5672bf6ea9a93b83e4f6279694b66fe3fecfc5
beltbunny05 <beltbunny.yt@proton.me>
2026-03-05 13:24:53 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let novoGasto = document.getElementById("Gasto");
+let categoria = document.getElementById("Categoria");
+let valordogasto = document.getElementById("Valor");
+
+
+
+function novo_gasto(){
+    const 
+}
+//Aprender como fazer o input sair dados para o tracker
+localStorage.setItem("","");
