b931ac5c0472aa573e67c7f81dbfe94fb62fdc13
beltbunny05 <beltbunny.yt@proton.me>
2026-03-07 16:13:59 -0300

    update on var and function

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
+// ---input---
 let novoGasto = document.getElementById("Gasto");
 let categoria = document.getElementById("Categoria");
 let valordogasto = document.getElementById("Valor");
+// ---texto---
+let mostrargasto = document.getElementById("texto") 
+// ---botao---
+let botaozao = document.getElementById("button1");
+// ------------
+botaozao.onclick = () => {
+    let valor_salvo = Number(novoGasto.value);
+};
 
 
 
-function novo_gasto(){
-    const 
-}
 //Aprender como fazer o input sair dados para o tracker
 localStorage.setItem("","");
