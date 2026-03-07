4d066e1c46897fd844a16987ba426b233112d448
beltbunny05 <beltbunny.yt@proton.me>
2026-03-07 18:31:14 -0300

    update on var and function

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 // ---input---
-let novoGasto = document.getElementById("Gasto");
+let input_gasto = document.getElementById("Gasto");
 let categoria = document.getElementById("Categoria");
 let valordogasto = document.getElementById("Valor");
 // ---texto---
 let mostrargasto = document.getElementById("texto") 
 // ---botao---
-let botaozao = document.getElementById("button1");
+let novo_gasto = document.getElementById("button1");
 // ------------
-botaozao.onclick = () => {
-    let valor_salvo = Number(novoGasto.value);
+novo_gasto.onclick = () => {
+    let valor_salvo = Number(input_gasto.value);
+    mostrargasto.textContent = valor_salvo;
 };
+
 
 
 
