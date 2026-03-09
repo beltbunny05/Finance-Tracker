b118f53871a4341093dabdf6eb0291b0ecdb5371
beltbunny05 <beltbunny.yt@proton.me>
2026-03-08 22:31:37 -0300

    JS update on Local Storage

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // ---input---
 let input_gasto = document.getElementById("Gasto");
 let categoria = document.getElementById("Categoria");
-let valordogasto = document.getElementById("Valor");
 // ---texto---
 let mostrargasto = document.getElementById("texto") 
 // ---botao---
@@ -10,10 +9,17 @@ let novo_gasto = document.getElementById("button1");
 novo_gasto.onclick = () => {
     let valor_salvo = Number(input_gasto.value);
     mostrargasto.textContent = valor_salvo;
+
+    localStorage.setItem("valor", valor_salvo);
 };
 
 
 
+const tracker =[
+    {"gasto":"0"},
+    {"categoria":"0"},
+]
 
 //Aprender como fazer o input sair dados para o tracker
 localStorage.setItem("","");
+localStorage.getItem("","");
